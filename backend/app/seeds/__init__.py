f61fc3a42d396061243fc36557e1cd3ac93ea2c2
Adam Scoggins <scoggins.aj@gmail.com>
2023-03-23 23:40:38 -0500

    render issues

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,7 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 
-from app.models.db import db, environment, SCHEMA
+from backend.app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
