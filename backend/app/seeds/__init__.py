7d59378e8da78291e38e20d428c1609c41f506a4
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-03-25 01:51:07 -0700

    add question model and seed file

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,5 +1,9 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .questions import seed_questions, undo_questions
+from .answers import seed_answers, undo_answers
+from .question_votes import seed_question_votes,undo_question_votes
+from .answer_votes import seed_answer_votes,undo_answer_votes
 
 from backend.app.models.db import db, environment, SCHEMA
 
@@ -12,13 +16,21 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_questions()
+        undo_answers()
+        undo_question_votes()
+        undo_answer_votes()
     seed_users()
     # Add other seed functions here
+    seed_questions()
+    seed_answers()
+    seed_question_votes()
+    seed_answer_votes()
 
 
 # Creates the `flask seed undo` command
@@ -26,3 +38,7 @@ def seed():
 def undo():
     undo_users()
     # Add other undo functions here
+    undo_questions()
+    undo_answers()
+    undo_question_votes()
+    undo_answer_votes()
---
+++ backend/app/seeds/questions.py
@@ -0,0 +1,26 @@
+from backend.app.models import db, Question, environment, SCHEMA
+from sqlalchemy.sql import text
+from datetime import datetime
+
+
+def seed_questions():
+    question1 = Question(
+        title='Why dogs are so cute', details='Why dogs are so cute',created_at =datetime.now(),updated_at=datetime.now(),user_id=1)
+    question2 = Question(
+        title='Why cats are so cute', details='Why cats are so cute',created_at =datetime.now(),updated_at=datetime.now(), user_id=2)
+    question3 = Question(
+        title='Why birds are so cute', details='Why birds are so cute',created_at =datetime.now(),updated_at=datetime.now(), user_id=3)
+
+    db.session.add(question1)
+    db.session.add(question2)
+    db.session.add(question3)
+    db.session.commit()
+
+
+def undo_questions():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.questions RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM questions"))
+
+    db.session.commit()
