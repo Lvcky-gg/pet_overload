1b371fc24e8395db0f17b90fb82d3668fa9d4a3b
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-03 23:18:25 -0500

    Cleaned up test questions tests

--- backend/app/tests/test_questions.py
+++ backend/app/tests/test_questions.py
@@ -1,25 +1,16 @@
-import pytest
 from ..models.question import Question
-from ..models.db import db as _db
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from .conftest import app
-
 
 def test_create_question(sqlalchemy_session):
-    # Create a new question and add it to the session
     question = Question(
-        title='Test Question',
-        details='This is a test question.',
-        user_id=1
+        title="Test Question", details="This is a test question.", user_id=1
     )
     sqlalchemy_session.add(question)
     sqlalchemy_session.commit()
 
-    # Retrieve the question from the database
-    retrieved_question = sqlalchemy_session.query(Question).filter_by(title='Test Question').first()
+    retrieved_question = (
+        sqlalchemy_session.query(Question).filter_by(title="Test Question").first()
+    )
 
-    # Check that the retrieved question has the expected values
-    assert retrieved_question.title == 'Test Question'
-    assert retrieved_question.details == 'This is a test question.'
+    assert retrieved_question.title == "Test Question"
+    assert retrieved_question.details == "This is a test question."
     assert retrieved_question.user_id == 1
