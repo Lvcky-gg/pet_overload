a1303acad4c3e7e4f1b6a3b5570dffec8cec8ac6
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-03 21:31:32 -0500

    fixed issues

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -13,8 +14,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
-    created_at =db.Column(db.DateTime,nullable=False)
-    updated_at =db.Column(db.DateTime,nullable=False)
+    created_at =db.Column(db.DateTime,nullable=False, default=datetime.utcnow())
+    updated_at =db.Column(db.DateTime,nullable=False, default=datetime.utcnow())
 
     #relationship
     user_questions=db.relationship("Question",back_populates="user")
