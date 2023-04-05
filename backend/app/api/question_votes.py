b633aec4d07155e786bfc8db9b527f6545540ea3
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-04-04 19:39:56 -0700

    add update&delete question_votes routes

--- backend/app/api/question_votes.py
+++ backend/app/api/question_votes.py
@@ -1,10 +1,49 @@
-from flask import Blueprint, jsonify, session, request
-from flask_login import current_user, login_user, logout_user, login_required
-from ..models import User, db, QuestionVote
+from flask import Blueprint,request
+from flask_login import current_user,login_required
+from ..models import QuestionVote
+from ..models.utils import success,not_found,bad_request,forbidden
 
 
 question_votes_routes = Blueprint('question_votes', __name__)
 
+
+@question_votes_routes.route("/<int:vote_id>",methods=['PUT'])
+@login_required
+def change_question_vote(vote_id):
+    '''
+    Update a current user's vote of a question by id
+    '''
+    is_liked=request.json.get("isLiked",None)
+    if is_liked is None:
+        return bad_request("Is_liked is required.")
+    if not isinstance(is_liked, bool):
+        return bad_request("Is_liked must be a boolean value.")
+
+    vote=QuestionVote.query.filter(QuestionVote.id==vote_id).first()
+    if vote is None:
+        return not_found("Vote couldn't be found.")
+    #check if vote belongs to current user
+    user_id=current_user.id
+    if not user_id==vote.user_id:
+        return forbidden("Forbidden")
+    updated_vote = vote.update_question_vote(is_liked=is_liked)
+    return updated_vote.to_dict()
+
+@question_votes_routes.route("/<int:vote_id>",methods=['DELETE'])
+@login_required
+def remove_question_vote(vote_id):
+    '''
+    Delete a current user's vote of a question by id
+    '''
+    vote=QuestionVote.query.filter(QuestionVote.id==vote_id).first()
+    if vote is None:
+        return not_found("Vote couldn't be found.")
+    #check if vote belongs to current user
+    user_id=current_user.id
+    if not user_id==vote.user_id:
+        return forbidden("Forbidden")
+    vote.delete_question_vote()
+    return success("Question_vote successfully deleted")
 
 @question_votes_routes.route("/current")
 @login_required
--- backend/app/models/question_vote.py
+++ backend/app/models/question_vote.py
@@ -1,4 +1,6 @@
 #question_vote.py
+from typing import Union
+from datetime import datetime
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
@@ -10,14 +12,72 @@ class QuestionVote(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     is_liked=db.Column(db.Boolean,nullable=False)
-    created_at =db.Column(db.DateTime,nullable=False)
-    updated_at =db.Column(db.DateTime,nullable=False)
-    user_id=db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
-    question_id=db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod("questions.id")),nullable=False)
+    created_at =db.Column(db.DateTime,nullable=False, default=datetime.utcnow())
+    updated_at =db.Column(db.DateTime,nullable=False, default=datetime.utcnow())
+    user_id=db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False
+        )
+    question_id=db.Column(
+        db.Integer,db.ForeignKey(add_prefix_for_prod("questions.id")),nullable=False
+        )
 
     #relationship
     user=db.relationship("User",back_populates="question_votes")
     question=db.relationship("Question",back_populates="question_votes")
+
+    @classmethod
+    def get_question_vote_by_id(cls, id: int, session=None) -> Union['QuestionVote', None]:
+        '''
+        Returns a question vote by id
+        '''
+
+        # If a session is provided (i.e. a test session from the sqlalchemy_session fixture), then use it to perform the query instead of the default session tied to the app context
+        if session is None:
+            return cls.query.filter_by(id=id).first()
+        else:
+            return session.query(cls).filter_by(id=id).first()
+
+
+    @classmethod
+    def get_question_vote_by_user_and_question(cls, user_id: int, question_id: int, session=None) -> Union['QuestionVote', None]:
+        '''
+        Returns a question vote by user_id and question_id
+        '''
+        if session is None:
+            return cls.query.filter_by(user_id=user_id, question_id=question_id).first()
+        else:
+            return session.query(cls).filter_by(user_id=user_id, question_id=question_id).first()
+
+    @classmethod
+    def add_question_vote(cls, is_liked: bool, user_id: int, question_id: int) -> 'QuestionVote':
+        '''
+        Adds a new question vote
+        '''
+        question_vote = cls(is_liked=is_liked, user_id=user_id, question_id=question_id)
+
+        db.session.add(question_vote)
+        db.session.commit()
+
+        return question_vote
+
+    def update_question_vote(self, is_liked: bool) -> 'QuestionVote':
+        '''
+        Updates an existing question vote
+        '''
+        self.is_liked = is_liked
+        self.updated_at = datetime.utcnow()
+
+        db.session.commit()
+
+        return self
+
+    def delete_question_vote(self) -> None:
+        '''
+        Deletes an existing question vote
+        '''
+        db.session.delete(self)
+        db.session.commit()
+
 
     def to_dict(self):
         return {
