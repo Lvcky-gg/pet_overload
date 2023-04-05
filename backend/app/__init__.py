0715846e998c525102349cb38093859a3d9ec301
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-04 19:18:04 -0500

    Formatted document

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,12 +14,13 @@ from .api.answer_votes import answer_votes_routes
 from .seeds import seed_commands
 from .config import Config
 
+
 def create_app():
-    app = Flask(__name__, static_folder='../../frontend/build', static_url_path='/')
+    app = Flask(__name__, static_folder="../../frontend/build", static_url_path="/")
 
     # Setup login manager
     login = LoginManager(app)
-    login.login_view = 'auth.unauthorized'
+    login.login_view = "auth.unauthorized"
 
     @login.user_loader
     def load_user(id):
@@ -29,12 +30,12 @@ def create_app():
     app.cli.add_command(seed_commands)
 
     app.config.from_object(Config)
-    app.register_blueprint(user_routes, url_prefix='/api/users')
-    app.register_blueprint(auth_routes, url_prefix='/api/auth')
-    app.register_blueprint(questions_routes_blueprint, url_prefix='/api/questions')
-    app.register_blueprint(answer_routes, url_prefix='/api/answer')
-    app.register_blueprint(question_votes_routes,url_prefix="/api/question_votes")
-    app.register_blueprint(answer_votes_routes,url_prefix="/api/answer_votes")
+    app.register_blueprint(user_routes, url_prefix="/api/users")
+    app.register_blueprint(auth_routes, url_prefix="/api/auth")
+    app.register_blueprint(questions_routes_blueprint, url_prefix="/api/questions")
+    app.register_blueprint(answer_routes, url_prefix="/api/answer")
+    app.register_blueprint(question_votes_routes, url_prefix="/api/question_votes")
+    app.register_blueprint(answer_votes_routes, url_prefix="/api/answer_votes")
     db.init_app(app)
     Migrate(app, db)
 
@@ -43,10 +44,12 @@ def create_app():
 
     return app
 
+
 app = create_app()
 
 # Application Security
 CORS(app)
+
 
 # Since we are deploying with Docker and Flask,
 # we won't be using a buildpack when we deploy to Heroku.
@@ -55,9 +58,9 @@ CORS(app)
 # Well.........
 @app.before_request
 def https_redirect():
-    if os.environ.get('FLASK_ENV') == 'production':
-        if request.headers.get('X-Forwarded-Proto') == 'http':
-            url = request.url.replace('http://', 'https://', 1)
+    if os.environ.get("FLASK_ENV") == "production":
+        if request.headers.get("X-Forwarded-Proto") == "http":
+            url = request.url.replace("http://", "https://", 1)
             code = 301
             return redirect(url, code=code)
 
@@ -65,12 +68,12 @@ def https_redirect():
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie(
-        'csrf_token',
+        "csrf_token",
         generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
-        samesite='Strict' if os.environ.get(
-            'FLASK_ENV') == 'production' else None,
-        httponly=True)
+        secure=True if os.environ.get("FLASK_ENV") == "production" else False,
+        samesite="Strict" if os.environ.get("FLASK_ENV") == "production" else None,
+        httponly=True,
+    )
     return response
 
 
@@ -79,26 +82,31 @@ def api_help():
     """
     Returns all API routes and their doc strings
     """
-    acceptable_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    route_list = { rule.rule: [[ method for method in rule.methods if method in acceptable_methods ],
-                    app.view_functions[rule.endpoint].__doc__ ]
-                    for rule in app.url_map.iter_rules() if rule.endpoint != 'static' }
+    acceptable_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    route_list = {
+        rule.rule: [
+            [method for method in rule.methods if method in acceptable_methods],
+            app.view_functions[rule.endpoint].__doc__,
+        ]
+        for rule in app.url_map.iter_rules()
+        if rule.endpoint != "static"
+    }
     return route_list
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def react_root(path):
     """
     This route will direct to the public directory in our
     react builds in the production environment for favicon
     or index.html requests
     """
-    if path == 'favicon.ico':
-        return app.send_from_directory('public', 'favicon.ico')
-    return app.send_static_file('index.html')
+    if path == "favicon.ico":
+        return app.send_from_directory("public", "favicon.ico")
+    return app.send_static_file("index.html")
 
 
 @app.errorhandler(404)
 def not_found(e):
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
