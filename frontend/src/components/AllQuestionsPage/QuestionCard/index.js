c8e737c4546dab68704b455913384bf56ecce2c2
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-08 15:29:28 -0500

    Cleaned up the styling and layout. Added the voting arrows along with animations, using FA

--- frontend/src/components/AllQuestionsPage/QuestionCard/index.js
+++ frontend/src/components/AllQuestionsPage/QuestionCard/index.js
@@ -1,15 +1,38 @@
+import { useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './QuestionCard.css';
 
 const QuestionCard = ({ title, details, votes_score, answers_count }) => {
     const answers_msg = answers_count === 1 ? 'answer' : 'answers';
+    const upvoteArrowRef = useRef(null);
+    const downvoteArrowRef = useRef(null);
+
+    const handleVoteArrowClick = (arrowRef) => {
+        arrowRef.current.classList.add('fa-beat');
+
+        setTimeout(() => {
+            arrowRef.current.classList.remove('fa-beat');
+        }, 800);
+    };
 
     return (
         <div className="question-card">
             <div className="row">
-                <div id="voting-score">
-                    <p>Up</p>
+                <div className="voting-score">
+                    <FontAwesomeIcon
+                        className="upvote-arrow"
+                        icon="fa-up-long"
+                        ref={upvoteArrowRef}
+                        onClick={() => handleVoteArrowClick(upvoteArrowRef)}
+                    />
                     <p className="voting-score-value">{votes_score}</p>
-                    <p>Down</p>
+                    <FontAwesomeIcon
+                        className="downvote-arrow"
+                        icon="fa-down-long"
+                        ref={downvoteArrowRef}
+                        onClick={() => handleVoteArrowClick(downvoteArrowRef)}
+                    />
                 </div>
 
                 <div className="title-description-col">
