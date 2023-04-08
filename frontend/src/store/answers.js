fff2284c38ead26d9cc9f3f4b1b765075e80b88c
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-08 17:33:57 -0500

    modified answers and users

--- frontend/src/store/answers.js
+++ frontend/src/store/answers.js
@@ -1,38 +1,57 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const answerSlice = createSlice({
-    name:'answers',
+    name: 'answers',
     initialState: {
         allAnswers: [],
     },
     reducers: {},
     extraReducers: (builder) => {
         builder
-        .addCase(getAllAnswers.fulfilled, (state, action)=> {
-            state.allAnswers = action.payload;
-        })
-        .addCase(getAllAnswers.rejected, (state, action)=> {
-            console.log('Rejected with value:', action.payload);
-        });
+            .addCase(getAllAnswers.fulfilled, (state, action) => {
+                state.allAnswers = action.payload;
+            })
+            .addCase(getAllAnswers.rejected, (state, action) => {
+                console.log('Rejected with value:', action.payload);
+            });
     },
 });
 
 export const getAllAnswers = createAsyncThunk(
     'answers/getAllAnswers',
-    async (_, {rejectWithValue}) => {
+    async (_, { rejectWithValue }) => {
         const response = await fetch('/api/answers', {
             headers: {
                 'Content-Type': 'application/json',
             },
         });
 
-        if(!response.ok) {
-            rejectWithValue(await response.json())
+        if (!response.ok) {
+            rejectWithValue(await response.json());
         }
-        const data = await response.json()
-        console.log('data', data)
+        const data = await response.json();
         return data.answers;
     }
 );
 
-export default answerSlice.reducer
+export const getAnswersByQuestion = createAsyncThunk(
+    'answers/getAnswersByQuestion',
+    async (questionId, { rejectWithValue }) => {
+        const response = await fetch(`/api/questions/${questionId}/answers`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            rejectWithValue(await response.json());
+        }
+
+        const data = await response.json();
+        console.log(`Answers to question ${questionId}`, data.answers);
+
+        return data.answers;
+    }
+);
+
+export default answerSlice.reducer;
