cd9f12eac4f6e50d245fa27bd8d844fc2d90bca9
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-09 04:17:17 -0500

    going to bed

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,11 @@
 import os
 
+database_url = os.environ.get('DATABASE_URL')
+schema = os.environ.get('SCHEMA')
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = f"{database_url}?options=-csearch_path={schema}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     
