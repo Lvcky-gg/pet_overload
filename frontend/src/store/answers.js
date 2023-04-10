0f4f25979031fa077c592c844371c58069550b9c
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-10 10:19:25 -0500

    no change

--- frontend/src/store/answers.js
+++ frontend/src/store/answers.js
@@ -1,38 +1,60 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const answerSlice = createSlice({
-    name:'answers',
+    name: 'answers',
     initialState: {
         allAnswers: [],
     },
     reducers: {},
     extraReducers: (builder) => {
         builder
-        .addCase(getAllAnswers.fulfilled, (state, action)=> {
-            state.allAnswers = action.payload;
-        })
-        .addCase(getAllAnswers.rejected, (state, action)=> {
-            console.log('Rejected with value:', action.payload);
-        });
+            .addCase(getAllAnswers.fulfilled, (state, action) => {
+                state.allAnswers = action.payload;
+            })
+            .addCase(getAllAnswers.rejected, (state, action) => {
+                console.log('Rejected with value:', action.payload);
+            });
     },
 });
 
 export const getAllAnswers = createAsyncThunk(
     'answers/getAllAnswers',
-    async (_, {rejectWithValue}) => {
-        const response = await fetch('/api/answers', {
+    async (_, { rejectWithValue }) => {
+        const response = await fetch('api/answers', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            //source of bug
+            // credentials: 'include',
+        });
+
+        if (!response.ok) {
+            rejectWithValue(await response.json());
+        }
+        const data = await response.json();
+        console.log('data', data);
+        return data.Answers;
+    }
+);
+
+export const getAnswersByQuestion = createAsyncThunk(
+    'answers/getAnswersByQuestion',
+    async (questionId, { rejectWithValue }) => {
+        const response = await fetch(`/api/questions/${questionId}/answers`, {
             headers: {
                 'Content-Type': 'application/json',
             },
         });
 
-        if(!response.ok) {
-            rejectWithValue(await response.json())
+        if (!response.ok) {
+            rejectWithValue(await response.json());
         }
-        const data = await response.json()
-        console.log('data', data)
-        return data.answers;
+
+        const data = await response.json();
+        console.log(`Answers to question ${questionId}`, data.answers);
+
+        return data.Answers;
     }
 );
 
-export default answerSlice.reducer
+export default answerSlice.reducer;
