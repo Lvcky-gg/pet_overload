56cc36e54206cd8d8d120cb9850ef502eee09ee9
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-10 10:37:46 -0500

    added some ways home

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -4,6 +4,7 @@ import { logout } from '../../store/session';
 import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
+import { NavLink } from 'react-router-dom';
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -47,6 +48,10 @@ function ProfileButton({ user }) {
                     <>
                         <li>{user.username}</li>
                         <li>{user.email}</li>
+                        <li><NavLink to='/'>Home</NavLink></li>
+                        <li><NavLink to='/all-questions'>Questions</NavLink></li>
+                        <li><NavLink to='/all-users'>Users</NavLink></li>
+                        <li><NavLink to='/team'>Our Team</NavLink></li>
                         <li>
                             <button className="modalButton" onClick={handleLogout}>Log Out</button>
                         </li>
