0f3dbd6069076f8832a84f78eae63f8191452ebc
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-04-10 03:38:57 -0700

    added delete button component

---
+++ frontend/src/components/UserProfile/ActivityLists/DeleteButton.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const DeleteButton = ({ type, id, onDelete, setIsDelete }) => {
+    const [showConfirmation, setShowConfirmation] = useState(false);
+
+    const handleDelete = () => {
+        setShowConfirmation(true);
+    };
+    const handleConfirm = () => {
+        onDelete(type, id);
+        setShowConfirmation(false);
+        setIsDelete((prev) => !prev);
+    };
+    const handleCancel = () => {
+        setShowConfirmation(false);
+    };
+    return (
+        <>
+            {!showConfirmation ? (
+                <button className="button" onClick={handleDelete}>
+                    Delete
+                </button>
+            ) : (
+                <div>
+                    <span>Are you sure?</span>
+                    <div>
+                        <button className="button" onClick={handleConfirm}>
+                            Confirm
+                        </button>
+                        <button className="button" onClick={handleCancel}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default DeleteButton;
