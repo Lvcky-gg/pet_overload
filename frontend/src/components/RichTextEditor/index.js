16a28119622ec26bb489343d9f72297cb047c19c
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-10 21:00:32 -0500

    rich text editor modified

--- frontend/src/components/RichTextEditor/index.js
+++ frontend/src/components/RichTextEditor/index.js
@@ -3,10 +3,19 @@ import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useState } from "react";
+import { convertToHTML } from 'draft-convert';
+import'./editor.css'
 
 const RichEditor = () => {
     const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
-    return (<div>
+    const [convertedContent, setConvertedContent] = useState(null);
+
+    useEffect(() => {
+        let html = convertToHTML(editorState.getCurrentContent());
+        setConvertedContent(html);
+      }, [editorState]);
+      
+    return (<div className='editor'>
         <Editor
         editorState={editorState}
         onEditorStateChange={setEditorState}
