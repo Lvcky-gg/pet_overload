8667f116cfe4e21f8f98b3b2abae6d29ae4d31a6
Iris06-cs <100994372+Iris06-cs@users.noreply.github.com>
2023-04-11 22:49:30 -0700

    resolved whitespace issue (#78)

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './footer.css';
-import logo from './petoverload-1__2__720.png';
+// import logo from './petoverload-1__2__720.png';
 
 export const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-img-container">
-                {' '}
-                <img src={logo} alt="logo"></img>
+                {/* set the img as container background instead */}
+                {/* <img src={logo} alt="logo"></img> */}
             </div>
             <h2>Copyright June 2022 Cohort </h2>
         </div>
