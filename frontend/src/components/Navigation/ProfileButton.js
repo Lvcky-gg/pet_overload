c47f66d8225450a72a374e2f41137e5e351743af
Adam S <scoggins.aj@gmail.com>
2023-04-12 01:29:25 -0500

    Fixed the profile button (#80)

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -49,10 +49,10 @@ function ProfileButton({ user }) {
                         <li>{user.username}</li>
                         <li>{user.email}</li>
                         <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                        <li>
                             <NavLink to="/user/profile">Profile</NavLink>
+                        </li>
+                        {/* <li>
+                            <NavLink to="/">Home</NavLink>
                         </li>
                         <li>
                             <NavLink to="/all-questions">Questions</NavLink>
@@ -62,7 +62,7 @@ function ProfileButton({ user }) {
                         </li>
                         <li>
                             <NavLink to="/team">Our Team</NavLink>
-                        </li>
+                        </li> */}
 
                         <li>
                             <button
