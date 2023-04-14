ae77ab41c018cd7932ca3f350f97349fab2bb8d5
John O'Donnell <94711072+Lvcky-gg@users.noreply.github.com>
2023-04-13 21:08:35 -0500

    Fixed issues from pr 94 (#95)
    * no change
    
    * no change
    
    * no change
    
    * pull prep
    
    * newPR
    
    * fixed
    
    ---------
    
    Co-authored-by: John O'Donnell <johnodonnell1997@icloud.com>

--- frontend/src/components/SpecificQuestionPage/index.js
+++ frontend/src/components/SpecificQuestionPage/index.js
@@ -7,20 +7,24 @@ import './SpecificQuestion.css';
 import AnswerCard from './AnswerCard';
 import RichEditor from '../RichTextEditor';
 import { createAnswerByQuestion } from '../../store/answers';
+import { getQuestionVotes } from '../../store/questionVotes';
 
 const { useSelector, useDispatch } = require('react-redux');
 
 const SpecificQuestion = () => {
-
-    const [richTextEditor, setRichTextEditor] = useState(false)
-
-    const [hidden, setHidden] = useState(false)
-    const [isDelete, setIsDelete] = useState(false);
-    
-
     const questions = useSelector((state) => state.questions.allQuestions);
     const loading = useSelector((state) => state.questions.loading);
     const answers = useSelector((state) => state.answers.allAnswers);
+    const questionVotes = useSelector((state)=>state.questionVotes.questionVotes);
+
+
+    const [richTextEditor, setRichTextEditor] = useState(false)
+    const [hidden, setHidden] = useState(false)
+    const [isDelete, setIsDelete] = useState(false);
+    const [voteClicked, setVoteClicked] = useState(false);
+    
+
+
     const dispatch = useDispatch();
 
     const { questionId } = useParams();
@@ -34,11 +38,13 @@ const SpecificQuestion = () => {
 
     useEffect(() => {
         dispatch(getAllQuestions());
+        dispatch(getQuestionVotes());
         if(answers.length === 0){
             setHidden(false)
             dispatch(getAllAnswers())
         }
 
+        
         dispatch(getAllAnswers())
     }, [isDelete, richTextEditor]);
 
@@ -55,12 +61,17 @@ const SpecificQuestion = () => {
     if (loading) {
         return null;
     }
+    
     let id;
     let title;
     let details;
     let votes_score;
     let answers_count;
     let user_id;
+    let created_at;
+    let updated_at;
+    let user;
+  
 
 
     const question = questions.filter((question)=>+questionId === question.id)
@@ -71,8 +82,13 @@ const SpecificQuestion = () => {
         votes_score= question[0].votes_count
         answers_count = question[0].answers_count
         user_id = question[0].user_id
-
+        created_at = question[0].created_at
+        updated_at = question[0].updated_at
+        user = question[0].user
     }
+
+console.log(question[0])
+
     const answer = answers.filter((answer)=> +questionId === answer.questionId)
 
 
@@ -80,9 +96,6 @@ const SpecificQuestion = () => {
         <>
             {question && (
                 <div className="container" id="Single-questions-container">
-                    {/* <div>
-                <h1>{title}</h1>
-            </div> */}
                     <div className="all-answer-header">
                         <h1>{title}</h1>
                         <div className="ask-question-container">
@@ -103,7 +116,7 @@ const SpecificQuestion = () => {
                             richTextEditor={richTextEditor}
                         />
                     )}
-           <QuestionCard
+           {question[0] &&<QuestionCard
            className="qCard"
             key={id}
             id={id}
@@ -111,8 +124,13 @@ const SpecificQuestion = () => {
             details={details}
             votes_score={votes_score}
             showAnswers={showAnswers}
-
-            answers_count={answers_count}></QuestionCard> 
+            answers_count={answers_count}
+            answers={answers}
+            updated_at={updated_at}
+            created_at={created_at}
+            user={user}
+            >
+                </QuestionCard> }
 
             <div className="answerHolder">
             {hidden && answer.map(
