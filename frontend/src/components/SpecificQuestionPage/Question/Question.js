102526dfad2b7220f757d2a643c3ba2fe9e8e66f
Iris06-cs <100994372+Iris06-cs@users.noreply.github.com>
2023-04-15 09:17:14 -0700

    Pr99 100 (#101)
    * added reducer actions
    
    * updated all questions page
    
    * specific question page
    
    * reset input area to empty after creating
    
    * edit create answers show updating
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: Adam Scoggins <scoggins.aj@gmail.com>

--- frontend/src/components/SpecificQuestionPage/Question/Question.js
+++ frontend/src/components/SpecificQuestionPage/Question/Question.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { getAllQuestions } from '../../../store/questions';
 import { authenticate } from '../../../store/session';
 import Voting from '../../AllQuestionsPage/Voting/Voting';
 import dateFormater from '../../../utils/dateFormater';
 import DeleteButton from '../../UserProfile/ActivityLists/DeleteButton';
 import parse from 'html-react-parser';
 import { deleteQuestion, updateQuestion } from '../../../store/questions';
+
 const { useSelector, useDispatch } = require('react-redux');
 const Question = ({ question, setIsDelete }) => {
     const navigate = useNavigate();
@@ -32,6 +32,7 @@ const Question = ({ question, setIsDelete }) => {
         setIsDelete((prev) => !prev);
         navigate('/all-questions/');
     };
+
     const handleEditClick = (e) => {
         e.preventDefault();
         dispatch(updateQuestion({ id, title, details }));
