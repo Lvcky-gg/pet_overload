ea09d61871d21da8ac59ac8ddf97b3ac37d9e76d
Adam Scoggins <scoggins.aj@gmail.com>
2023-04-15 17:20:43 -0500

    Fixed edit question styling bug

--- frontend/src/components/AskAQuestionPage/QuestionForm/QuestionRichEditor/index.js
+++ frontend/src/components/AskAQuestionPage/QuestionForm/QuestionRichEditor/index.js
@@ -1,17 +1,25 @@
 // QuestionRichEditor/index.js
 import React, { useEffect, useState } from 'react';
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import { convertFromHTML } from 'draft-convert';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import draftToHtml from 'draftjs-to-html';
 
 import './QuestionRichEditor.css';
+import htmlToDraft from 'html-to-draftjs';
 
 const QuestionRichEditor = ({ details, setDetails }) => {
-    const [editorState, setEditorState] = useState(() =>
-        EditorState.createWithContent(convertFromHTML(details))
-    );
+    const [editorState, setEditorState] = useState(() => {
+        const { contentBlocks, entityMap } = htmlToDraft(details);
+
+        const contentState = ContentState.createFromBlockArray(
+            contentBlocks,
+            entityMap
+        );
+
+        return EditorState.createWithContent(contentState);
+    });
 
     useEffect(() => {
         if (editorState) {
